f5e4928b2fb0699f727ecbb3f69d5619abee7f69
stevver <stever.heinsaar@hkhk.edu.ee>
2024-02-06 14:02:37 +0200

    Create Tkinter01.py

---
+++ Tkinter/Tkinter01.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+aken = Tk()
+aken.title("S.Heinsaar")
+aken.resizable(0,0)
+aken.configure(background="black")
+tekst = Label(aken, pady="20", padx="20", fg="red", bg="black", font="Tahoma 16 bold italic", text="Nimi: Stever Heinsaar\nRühm: IT23\n2024").pack()
+
+
+aken.mainloop()
